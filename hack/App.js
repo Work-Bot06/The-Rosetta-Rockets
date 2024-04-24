040f112d689e4334982ff412dc7edcedd55de5d2
alrinajabin <53832403+alrin-a-jabin@users.noreply.github.com>
2024-04-24 14:20:10 +0530

    dmeo

--- hack/App.js
+++ hack/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hack</Text>
+      <Text>hack check work flow</Text>
       <StatusBar style="auto" />
     </View>
   );
